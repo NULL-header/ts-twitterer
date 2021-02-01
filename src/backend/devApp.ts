5f4b330cf74c93197744cf6c5ca720eadb34dc70
NULL-header <poaoq.poaoq@gmail.com>
2021-02-01 03:58:02 +0000

    add: dispersal backend code #10

---
+++ src/backend/devApp.ts
@@ -0,0 +1,14 @@
+import webpack from "webpack";
+import webpackDevMiddleware from "webpack-dev-middleware";
+import webpackHotMiddleware from "webpack-hot-middleware";
+import getConfigDev from "webpack.config.dev";
+import { app } from "./app";
+
+const config = getConfigDev({ production: false, development: true });
+const compiler = webpack(config);
+
+export const devApp = app
+  .use(
+    webpackDevMiddleware(compiler, { publicPath: config.output?.publicPath })
+  )
+  .use(webpackHotMiddleware(compiler));
