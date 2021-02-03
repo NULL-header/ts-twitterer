e756188e7816ccd8ab44afbd2d849042995a30d6
NULL-header <poaoq.poaoq@gmail.com>
2021-02-03 05:38:01 +0000

    update: adjust types #21

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,14 +1,29 @@
 /* eslint-disable camelcase */
 interface Tweet {
   id: number;
+  dataid: number;
   username: string;
   userid: string;
   iconUrl: string;
   content: string;
+  createdAt: string;
+}
+interface TweetColumns {
+  id: number;
+  dataid: number;
+  username: string;
+  userid: string;
+  icon_url: string;
+  content: string;
   created_at: string;
 }
 
-interface Config {
-  id: number;
+interface Configs {
+  lastTweetId: number;
+  newestTweetDataId: number;
+}
+interface ConfigColumns {
+  id: 0;
   last_tweet_id: number;
+  newest_tweet_data_id: number;
 }
