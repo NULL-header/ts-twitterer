c218cfc8ac91f12607bce9f18bf8ad623b22faf3
NULL-header <poaoq.poaoq@gmail.com>
2021-02-04 11:24:37 +0000

    update: use the jss #36

--- src/frontend/components/Tweet/Tweet.tsx
+++ src/frontend/components/Tweet/Tweet.tsx
@@ -1,19 +1,25 @@
 import React from "react";
+import { useStyles } from "./style";
 
 export const Tweet: React.FC<{ tweet: Tweet }> = React.memo((props) => {
   console.log(props.tweet.media);
+  const classes = useStyles();
   return (
-    <div>
-      <img src={props.tweet.iconUrl} alt="icon"></img>
-      <div>
-        <span>{props.tweet.username}</span>
-        <span>{props.tweet.userid}</span>
+    <div className={classes.root}>
+      <div className={classes.tweetContainer}>
+        <img src={props.tweet.iconUrl} className={classes.icon}></img>
+        <div className={classes.user}>
+          <div>{props.tweet.username}</div>
+          <div>{"@" + props.tweet.userid}</div>
+        </div>
+        <div className={classes.tweetContent}>{props.tweet.content}</div>
       </div>
-      <div>{props.tweet.content}</div>
-      {props.tweet.media &&
-        props.tweet.media.map((e, i) => (
-          <img src={e.mediaUrl} alt={"image" + i} key={i}></img>
-        ))}
+      <div className={classes.image}>
+        {props.tweet.media &&
+          props.tweet.media.map((e, i) => (
+            <img src={e.mediaUrl} alt={"image" + i} key={i}></img>
+          ))}
+      </div>
     </div>
   );
 });
