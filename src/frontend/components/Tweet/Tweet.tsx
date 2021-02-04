be955096fe56ae303845ee0e86cac5878c55d4ec
NULL-header <poaoq.poaoq@gmail.com>
2021-02-04 07:38:17 +0000

    update: show images #38

--- src/frontend/components/Tweet/Tweet.tsx
+++ src/frontend/components/Tweet/Tweet.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export const Tweet: React.FC<{ tweet: Tweet }> = React.memo((props) => {
+  console.log(props.tweet.media);
   return (
     <div>
       <img src={props.tweet.iconUrl} alt="icon"></img>
@@ -9,6 +10,10 @@ export const Tweet: React.FC<{ tweet: Tweet }> = React.memo((props) => {
         <span>{props.tweet.userid}</span>
       </div>
       <div>{props.tweet.content}</div>
+      {props.tweet.media &&
+        props.tweet.media.map((e, i) => (
+          <img src={e.mediaUrl} alt={"image" + i} key={i}></img>
+        ))}
     </div>
   );
 });
