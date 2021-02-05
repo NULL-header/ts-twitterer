bcce3655ae865411aaa7705e86dc69eae727ff6a
NULL-header <poaoq.poaoq@gmail.com>
2021-02-05 16:08:44 +0000

    update: new rule for sample file name #46

--- src/backend/CONSTVALUE.ts
+++ src/backend/CONSTVALUE.ts
@@ -9,5 +9,6 @@ export const CONSTVALUE = {
   CONSUMER_TOKEN_SECRET: process.env.CONSUMER_TOKEN_SECRET as string,
   SAMPLE_LIST_ID: process.env.SAMPLE_LIST_ID as string,
   PORT: (parseInt(process.env.port as string) || 3000) as number,
-  SAMPLE_FILE_PATH: process.cwd() + "/tests/sampleTweets/sample.json",
+  SAMPLE_DIRECTORY: process.cwd() + "/tests/sampleTweets/",
+  SAMPLE_BASE_NAME: "sample",
 };
