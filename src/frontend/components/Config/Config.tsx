0a1688df7e8e9c66c33fb4ddf5f5ede99b126064
NULL-header <poaoq.poaoq@gmail.com>
2021-02-06 06:35:57 +0000

    update: initialize about listId #50

--- src/frontend/components/Config/Config.tsx
+++ src/frontend/components/Config/Config.tsx
@@ -3,6 +3,11 @@ import { useSelector, useUpdate } from "src/frontend/globalState";
 
 export const Config: React.FC = React.memo(() => {
   const listIds = useSelector((state) => state.listIds);
+  const newestTweetDataIdGroup = useSelector(
+    (state) => state.newestTweetDataIdGroup
+  );
+  const lastTweetIdGroup = useSelector((state) => state.lastTweetIdGroup);
+  const tweetGroup = useSelector((state) => state.tweetGroup);
   const inputRef = React.useRef<HTMLInputElement | null>(null);
   const dispatch = useUpdate();
   const handleSubmit = React.useCallback(
@@ -11,10 +16,18 @@ export const Config: React.FC = React.memo(() => {
       if (inputRef.current == null) return;
       const listId = inputRef.current.value;
       if (listId.length === 0) return;
-      dispatch({ type: "MODIFY", state: { listIds: [...listIds, listId] } });
+      dispatch({
+        type: "MODIFY",
+        state: {
+          listIds: [...listIds, listId],
+          newestTweetDataIdGroup: { ...newestTweetDataIdGroup, [listId]: "0" },
+          lastTweetIdGroup: { ...lastTweetIdGroup, [listId]: 0 },
+          tweetGroup: { ...tweetGroup, [listId]: [] },
+        },
+      });
       inputRef.current.value = "";
     },
-    [dispatch, listIds]
+    [dispatch, lastTweetIdGroup, listIds, newestTweetDataIdGroup, tweetGroup]
   );
   console.log({ listIds });
   return (
