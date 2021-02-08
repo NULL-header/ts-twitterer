d2c5746c7048c69ffd277d11242ac5d55c052d37
NULL-header <poaoq.poaoq@gmail.com>
2021-02-08 01:17:01 +0000

    update: getting data #60

--- src/frontend/globalState/makeTweets.ts
+++ src/frontend/globalState/makeTweets.ts
@@ -2,30 +2,53 @@ export const makeTweets = async (
   tweetLows: TweetColumns[]
 ): Promise<Tweet[]> => {
   return tweetLows.map((e) => {
-    const {
-      content,
-      media: mediaColumns,
-      id,
-      created_at: createdAt,
-      icon_url: iconUrl,
-      userid,
-      dataid,
-      username,
-      list_id: listId,
-    } = e;
-    const media: Media | undefined = mediaColumns
-      ? { type: mediaColumns.type, mediaUrl: mediaColumns.media_url as any }
-      : undefined;
-    return {
-      content,
-      media,
-      id,
-      createdAt,
-      iconUrl,
-      userid,
-      dataid,
-      username,
-      listId,
-    };
+    const tweet = extractData(e);
+    tweet.media = makeMedia(e.media);
+    return tweet;
   });
+};
+
+const makeMedia = (mediaColumns?: MediaColumns) => {
+  if (mediaColumns == null) return undefined;
+  const { type: mediaType, media_url: mediaUrl } = mediaColumns;
+  return { type: mediaType, mediaUrl } as Media;
+};
+
+const extractData = (tweetColumns: TweetColumns): Tweet => {
+  const {
+    content,
+    id,
+    created_at: createdAt,
+    icon_url: iconUrl,
+    userid,
+    dataid,
+    username,
+    list_id: listId,
+    is_retweeted: isRetweeted,
+  } = tweetColumns;
+  const retweeterName = (tweetColumns as any).retweeter_name as
+    | string
+    | undefined;
+  const retweetData = makeRetweetData(isRetweeted, retweeterName);
+  const tweet: Tweet = {
+    content,
+    id,
+    createdAt,
+    iconUrl,
+    userid,
+    username,
+    dataid,
+    listId,
+    ...retweetData,
+  };
+  return tweet;
+};
+
+const makeRetweetData = (isRetweeted: boolean, retweeterName?: string) => {
+  if (isRetweeted) {
+    if (retweeterName == null) throw new Error("retweeterName is undefined");
+    return { isRetweeted, retweeterName };
+  } else {
+    return { isRetweeted };
+  }
 };
