35a4cd70d6534bc445a690fb07005012c59b204d
NULL-header <poaoq.poaoq@gmail.com>
2021-02-08 05:43:52 +0000

    add: style for config #64

---
+++ src/frontend/components/Config/style.ts
@@ -0,0 +1,11 @@
+import { makeStyles } from "src/frontend/theme";
+export const useStyles = makeStyles()({
+  root: {
+    borderStyle: "none solid",
+    borderColor: "lightgrey",
+    padding: 10,
+  },
+  divider: {
+    color: "lightgrey",
+  },
+});
