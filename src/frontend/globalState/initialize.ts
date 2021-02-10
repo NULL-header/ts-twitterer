494eb734595a76b81b086cd65c1fba982fb2f416
NULL-header <poaoq.poaoq@gmail.com>
2021-02-10 08:58:27 +0000

    add: process of initialize #81

---
+++ src/frontend/globalState/initialize.ts
@@ -0,0 +1,17 @@
+import { loadConfigs } from "./config";
+import { loadOldTweets } from "./tweets";
+
+export const initialize = async () => {
+  const config = await loadConfigs();
+  if (config == null) throw new Error("last data of config does not exist");
+  const oldTweetsArray = await Promise.all(
+    config.listIds.map((listId) =>
+      loadOldTweets(config.lastTweetIdGroup[listId], listId)
+    )
+  );
+  const oldTweetGroup = config.listIds.reduce((a, listId, i) => {
+    a[listId] = oldTweetsArray[i];
+    return a;
+  }, {} as Record<string, Tweet[]>);
+  return { ...config, tweetGroup: oldTweetGroup };
+};
