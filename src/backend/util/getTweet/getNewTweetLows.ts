36eea37c97888ca3e21212e7be5e4b87be7fc758
NULL-header <poaoq.poaoq@gmail.com>
2021-02-11 16:33:00 +0000

    update: dispersal logic #78

--- src/backend/util/getTweet/getNewTweetLows.ts
+++ src/backend/util/getTweet/getNewTweetLows.ts
@@ -6,13 +6,7 @@ export const getNewTweetLows = async (
   listId: string
 ) => {
   console.log("getNew");
-  const response = (await twitterApi
-    .get("lists/statuses", {
-      list_id: listId,
-      include_rts: true,
-      tweet_mode: "extended",
-    })
-    .catch((err) => console.log(err))) as any[];
+  const response = await getNewTweetData(listId);
   const lastIndexOldTweetData = (response as any[]).findIndex(
     (e) => e.id === lastNewestTweetDataId
   );
@@ -20,5 +14,28 @@ export const getNewTweetLows = async (
     lastIndexOldTweetData < 0
       ? response
       : response.slice(lastIndexOldTweetData + 1);
+  return newTweetData.map((e) => makeTweetLow(e, listId)).reverse();
+};
+
+export const getNewTweetData = (listId: string): Promise<any[]> => {
+  return twitterApi.get("lists/statuses", {
+    list_id: listId,
+    include_rts: true,
+    tweet_mode: "extended",
+  }) as Promise<any[]>;
+};
+
+export const makeTweetLows = (
+  tweetData: any[],
+  listId: string,
+  lastNewestTweetDataId: string
+) => {
+  const lastIndexOldTweetData = tweetData.findIndex(
+    (e) => e.id === lastNewestTweetDataId
+  );
+  const newTweetData: any[] =
+    lastIndexOldTweetData < 0
+      ? tweetData
+      : tweetData.slice(lastIndexOldTweetData + 1);
   return newTweetData.map((e) => makeTweetLow(e, listId)).reverse();
 };
