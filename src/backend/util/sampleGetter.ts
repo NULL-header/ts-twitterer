c1416c233e0357f6f662547d3438fcfa95ea7922
NULL-header <poaoq.poaoq@gmail.com>
2021-02-11 16:44:21 +0000

    add: maker of sample getter #78

---
+++ src/backend/util/sampleGetter.ts
@@ -0,0 +1,90 @@
+import { promises as fsOrigin } from "fs";
+import { getRateLimitData, makeLimitData } from "./getRateLimit";
+import { getNewTweetData, makeTweetLows } from "./getTweet";
+import { CONSTVALUE } from "../CONSTVALUE";
+
+const makeSampleGetter = <
+  GetterArgs extends Array<any>,
+  MakerArgs extends Array<any>,
+  Result
+>(
+  getter: (...args: GetterArgs) => Promise<any>,
+  maker: (arg: any, ...args: MakerArgs) => Result,
+  basePath: string
+) => {
+  const fs = makeFs(basePath);
+  const utils = makeUtil(fs);
+  const adjustedUtil = utils("adjusted");
+  const rawUtil = utils("raw");
+  return async (
+    args: { getter: GetterArgs; maker: MakerArgs },
+    doesUpdate: boolean,
+    additionalPath?: string
+  ): Promise<Result> => {
+    const path = additionalPath == null ? "" : "-" + additionalPath;
+
+    const adjustedSample = await adjustedUtil.read(path);
+    if (adjustedSample != null && !doesUpdate) return adjustedSample as Result;
+
+    const rawSample = await rawUtil.read(path);
+    if (rawSample != null && !doesUpdate) {
+      const adjustedSample = maker(rawSample, ...args.maker);
+      await adjustedUtil.write(path, JSON.stringify(adjustedSample));
+      return adjustedSample;
+    } else {
+      const rawSample = await getter(...args.getter);
+      const promiseRaw = rawUtil.write(path, JSON.stringify(rawSample));
+      const adjustedSample = maker(rawSample, ...args.maker);
+      const promiseAdjusted = adjustedUtil.write(
+        path,
+        JSON.stringify(adjustedSample)
+      );
+      await Promise.all([promiseRaw, promiseAdjusted]);
+      return adjustedSample;
+    }
+  };
+};
+
+const makeFs = (basePath: string) => ({
+  read: async (additionalPath: string) =>
+    await fsOrigin
+      .readFile(basePath + additionalPath, { encoding: "utf-8" })
+      .then(JSON.parse)
+      .catch((_err) => undefined),
+  write: async (additionalPath: string, target: string) =>
+    await fsOrigin
+      .writeFile(basePath + additionalPath, target, {
+        encoding: "utf-8",
+      })
+      .catch((_err) => undefined),
+});
+
+type Fs = ReturnType<typeof makeFs>;
+
+const makeUtil = (fs: Fs) => (suffix: string): Fs => {
+  const underPath = `-${suffix}.json`;
+  return {
+    read: async (path: string) => fs.read(path + underPath),
+    write: async (path: string, target: string) =>
+      fs.write(path + underPath, target),
+  };
+};
+
+const SAMPLE_BASE_PATH =
+  CONSTVALUE.SAMPLE_DIRECTORY + CONSTVALUE.SAMPLE_BASE_NAME;
+
+const RATE_PATH = SAMPLE_BASE_PATH + "-rate";
+
+export const getSampleRate = makeSampleGetter(
+  getRateLimitData,
+  makeLimitData,
+  RATE_PATH
+);
+
+const TWEET_PATH = SAMPLE_BASE_PATH + "-tweet";
+
+export const getSampleTweet = makeSampleGetter(
+  getNewTweetData,
+  makeTweetLows,
+  TWEET_PATH
+);
