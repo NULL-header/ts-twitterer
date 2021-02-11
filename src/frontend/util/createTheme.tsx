e6289110b9b9cc9345e0e4a94b8615d8f6384dd1
NULL-header <poaoq.poaoq@gmail.com>
2021-02-11 02:56:39 +0000

    add: theme creator #75

---
+++ src/frontend/util/createTheme.tsx
@@ -0,0 +1,24 @@
+import React, { createContext } from "react";
+import { createTheming } from "react-jss";
+
+import { createMakeStyles } from "./createMakeStyles";
+
+export const createTheme = <Theme, ThemeNames extends string>(
+  themes: Record<ThemeNames, Theme>
+) => {
+  const ThemeContext = createContext({} as Theme);
+  const Theming = createTheming(ThemeContext);
+  const makeStyles = createMakeStyles(Theming);
+  const ThemeProvider: React.FC<{
+    themeName: ThemeNames;
+    children: React.ReactElement;
+  }> = React.memo((props) => {
+    const theme = themes[props.themeName];
+    return (
+      <Theming.ThemeProvider theme={theme as any}>
+        {props.children}
+      </Theming.ThemeProvider>
+    );
+  });
+  return { ThemeProvider, makeStyles, Theming };
+};
