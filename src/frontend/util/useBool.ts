0e15d2367a067d88a66258961cefad25287c7d99
NULL-header <poaoq.poaoq@gmail.com>
2021-03-03 14:25:54 +0000

    add: hook of boolean state #119

---
+++ src/frontend/util/useBool.ts
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+export const useBool = (
+  initialValue: boolean,
+): [boolean, () => void, () => void] => {
+  const [value, setValue] = useState(initialValue);
+  return [value, () => setValue(true), () => setValue(false)];
+};
