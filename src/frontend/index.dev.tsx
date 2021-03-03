ab8913c770ba3ce635e1b2916c6b928ed97f32b4
NULL-header <poaoq.poaoq@gmail.com>
2021-03-03 12:01:56 +0000

    update: remove dev component #118

--- src/frontend/index.dev.tsx
+++ src/frontend/index.dev.tsx
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { App } from "./app";
-import { UpdateButton } from "./components/ForDev";
 
 ReactDOM.render(
   <>
     <App />
-    <UpdateButton />
   </>,
   document.getElementById("root"),
 );
