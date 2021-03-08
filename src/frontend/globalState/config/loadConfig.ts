2891e3e17be1c0089c203e65254543dbc80904f1
NULL-header <poaoq.poaoq@gmail.com>
2021-03-08 00:51:32 +0000

    update: unused files #143

--- src/frontend/globalState/config/loadConfig.ts
+++
@@ -1,9 +0,0 @@
-import { db } from "../../db";
-
-export const loadConfigs = async () => {
-  const dataNullable = await db.configs.get(0);
-  if (dataNullable != null) {
-    return dataNullable.last_data;
-  }
-  return undefined;
-};
