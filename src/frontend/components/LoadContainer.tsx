54ac5ece2db7d2ad7b51bcaeb4d7e5bda8cdbb97
NULL-header <poaoq.poaoq@gmail.com>
2021-03-09 13:44:43 +0000

    update: lazy import #145

--- src/frontend/components/LoadContainer.tsx
+++ src/frontend/components/LoadContainer.tsx
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useEffect, FC } from "react";
+import { useReducerAsync, AsyncActionHandlers } from "use-reducer-async";
 import { useTracked } from "frontend/globalState";
-import { ScreenContainer } from "./ScreenContainer";
 
-export const LoadContainer: React.FC = () => {
-  const [state] = useTracked();
-  if (state.isInitializing == null) return <div>Starting</div>;
-  if (state.isInitializing) return <div>Loading</div>;
-  return <ScreenContainer />;
+type Action = { type: "MODIFY"; Component: FC };
+type AsyncAction = { type: "LOAD_AUTH" } | { type: "LOAD_MAIN" };
+
+type State = FC | undefined;
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case "MODIFY": {
+      return action.Component as State;
+    }
+    default: {
+      throw new Error("An error occurred to modify the global state");
+    }
+  }
+};
+
+const asyncReducer: AsyncActionHandlers<typeof reducer, AsyncAction> = {
+  LOAD_AUTH: ({ dispatch, signal }) => async () => {
+    const { Component } = await import(/* webpackChunkName: "auth" */ "./Auth");
+    if (signal.aborted) return;
+    dispatch({ type: "MODIFY", Component });
+  },
+  LOAD_MAIN: ({ dispatch, signal }) => async () => {
+    const { Component } = await import(/* webpackChunkName: "main" */ "./main");
+    if (signal.aborted) return;
+    dispatch({ type: "MODIFY", Component });
+  },
+};
+
+export const LoadContainer = () => {
+  const [{ isInitializing, isAuthorized }] = useTracked();
+  const [Component, dispatch] = useReducerAsync(
+    reducer,
+    undefined,
+    asyncReducer,
+  );
+
+  useEffect(() => {
+    if (isInitializing == null || !isInitializing) return;
+    if (isAuthorized) dispatch({ type: "LOAD_MAIN" });
+    else dispatch({ type: "LOAD_AUTH" });
+  }, [isAuthorized, isInitializing]);
+
+  if (isInitializing == null) return <div>Starting</div>;
+  if (isInitializing) return <div>Loading</div>;
+  if (Component != null) return <Component />;
+  return <div>unknown error</div>;
 };
