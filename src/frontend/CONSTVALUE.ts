09ebe227749ded08d3195b9a9bd735598c4dcfdf
NULL-header <poaoq.poaoq@gmail.com>
2021-03-15 03:07:45 +0000

    update: remove mock function for api #133

--- src/frontend/CONSTVALUE.ts
+++ src/frontend/CONSTVALUE.ts
@@ -1,6 +1,6 @@
 export const CONSTVALUE = {
-  GET_TWEETS_URL: process.env.getTweetsUrl as string,
-  GET_RATE_URL: process.env.getRateUrl as string,
+  GET_TWEETS_URL: "/api/tweet",
+  GET_RATE_URL: "/api/rate",
   LIST_LIMIT: 3,
   LATE_LIMIT: 100,
 };
