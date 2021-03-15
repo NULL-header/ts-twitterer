2a8d5351c524448176ff9c25e6faa61b390e5dc4
NULL-header <poaoq.poaoq@gmail.com>
2021-03-15 02:32:26 +0000

    remove: unused files #142

--- src/frontend/models/GlobalState.ts
+++
@@ -1,9 +0,0 @@
-import Immutable from "immutable";
-
-const initValue = {
-  isAuthorized: false,
-};
-
-const BaseRecord = Immutable.Record(initValue);
-
-export class GlobalState extends BaseRecord {}
--- src/frontend/worker/connect/types.ts
+++
@@ -1,3 +0,0 @@
-import { State } from "frontend/globalState/types";
-
-export type Work = (args: State) => Promise<State | undefined>;
