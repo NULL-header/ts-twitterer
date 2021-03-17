854a7c84d5fc9ea5f9484954581793fd7c4adf68
NULL-header <poaoq.poaoq@gmail.com>
2021-03-17 05:26:35 +0000

    add: declare for node-fetch-cookies #133

---
+++ src/typings/lib/node-fetch-cookies.d.ts
@@ -0,0 +1,11 @@
+import "node-fetch-cookies";
+
+declare module "node-fetch-cookies" {
+  type NodeFetch = typeof import("node-fetch").default;
+  export class CookieJar {}
+  type FetchCookies = (
+    cookieJar: CookieJar,
+    ...args: NodeFetch extends (...nodeFetchArgs: infer A) => any ? A : never
+  ) => ReturnType<NodeFetch>;
+  export const fetch: FetchCookies;
+}
