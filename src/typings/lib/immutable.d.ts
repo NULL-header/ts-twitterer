a461e3e910157bbdd2edeffc453f97ca55835f54
NULL-header <poaoq.poaoq@gmail.com>
2021-03-27 07:39:04 +0000

    update: type for toJS #157

--- src/typings/lib/immutable.d.ts
+++ src/typings/lib/immutable.d.ts
@@ -1,4 +1,44 @@
-import "immutable";
+import Immutable from "immutable";
+
+type ArrayBranch = "ArrayBranch";
+type MapBranch = "MapBranch";
+
+type RecursiveImmutable<
+  T extends
+    | Record<string, any>
+    | Record<ArrayBranch, any>
+    | Record<MapBranch, any>
+> = T extends Record<ArrayBranch, infer A>
+  ? A extends Immutable.Record<infer B>
+    ? RecursiveImmutable<B>
+    : A extends Immutable.List<infer B>
+    ? RecursiveImmutable<Record<ArrayBranch, B>>[]
+    : A extends Immutable.Map<infer B, infer C>
+    ? B extends string
+      ? Record<B, RecursiveImmutable<Record<MapBranch, C>>>
+      : never
+    : A
+  : T extends Record<MapBranch, infer A>
+  ? A extends Immutable.Record<infer B>
+    ? RecursiveImmutable<B>
+    : A extends Immutable.List<infer B>
+    ? RecursiveImmutable<Record<MapBranch, B>>[]
+    : A extends Immutable.Map<infer B, infer C>
+    ? B extends string
+      ? Record<B, RecursiveImmutable<Record<MapBranch, C>>>
+      : never
+    : A
+  : {
+      [P in keyof T]: T[P] extends Immutable.Record<infer A>
+        ? RecursiveImmutable<A>
+        : T[P] extends Immutable.List<infer A>
+        ? RecursiveImmutable<Record<ArrayBranch, A>>[]
+        : T[P] extends Immutable.Map<infer A, infer B>
+        ? A extends string
+          ? Record<A, RecursiveImmutable<Record<MapBranch, B>>>
+          : never
+        : T[P];
+    };
 
 declare module "immutable" {
   interface List<T> {
@@ -9,6 +49,9 @@ declare module "immutable" {
   }
   // eslint-disable-next-line @typescript-eslint/ban-types
   interface Record<TProps extends Object> {
-    toJS(): TProps;
+    // toJS(): {
+    // [P in keyof TProps]: TProps[P] extends List<infer A> ? A[] : TProps[P];
+    // };
+    toJS(): RecursiveImmutable<TProps>;
   }
 }
